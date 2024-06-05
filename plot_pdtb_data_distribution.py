6b4bdf38bb36b7f3668a6f488f4f3c55522b042e
Nelson Filipe Costa <nelsoncosta.jr17@gmail.com>
2024-06-05 17:42:16 -0400

    modified values in text boxes

--- plot_pdtb_data_distribution.py
+++ plot_pdtb_data_distribution.py
@@ -20,21 +20,21 @@ axis_1.tick_params(axis='x', rotation=90)
 axis_1.set_ylim((0, 4500))
 axis_1.set_yticks(np.arange(0, 4501, 500))
 axis_1.bar(df['sense_final'].value_counts().index, df['sense_final'].value_counts(), color='tab:green')
-axis_1.text(h_offset, v_offset, 'Total Implicit DR:  21,730 \n One-Sense Implitcit DR:  20,658 \n Two-Senses Implicit DR:    1,072 ',
+axis_1.text(h_offset, v_offset, 'Total Implicit DR:  21,559 \n One-Sense Implitcit DR:  20,509 \n Two-Senses Implicit DR:    1,050 ',
             ha='right', va='top', transform=axis_1.transAxes, bbox=dict(boxstyle='square', facecolor='tab:green', alpha=0.1))
 
 axis_2.set_title(f'Lin Test Split', fontweight='bold')
 axis_2.tick_params(axis='x', rotation=90)
 axis_2.set_ylim((0, 200))
 axis_2.bar(df_lin['sense_final'].value_counts().index, df_lin['sense_final'].value_counts(), color='tab:red')
-axis_2.text(h_offset, v_offset, 'Total Implicit DR:  1,004 \n One-Sense Implitcit DR:     968 \n Two-Senses Implicit DR:       36 ',
+axis_2.text(h_offset, v_offset, 'Total Implicit DR:   995 \n One-Sense Implitcit DR:     959 \n Two-Senses Implicit DR:       36 ',
             ha='right', va='top', transform=axis_2.transAxes, bbox=dict(boxstyle='square', facecolor='tab:red', alpha=0.1))
 
 axis_3.set_title(f'Ji Test Split', fontweight='bold')
 axis_3.tick_params(axis='x', rotation=90)
 axis_3.set_ylim((0, 250))
 axis_3.bar(df_ji['sense_final'].value_counts().index, df_ji['sense_final'].value_counts(), color='tab:blue')
-axis_3.text(h_offset, v_offset, 'Total Implicit DR:  1,466 \n One-Sense Implitcit DR:  1,399 \n Two-Senses Implicit DR:       67 ',
+axis_3.text(h_offset, v_offset, 'Total Implicit DR:  1,453 \n One-Sense Implitcit DR:  1,387 \n Two-Senses Implicit DR:       66 ',
              ha='right', va='top', transform=axis_3.transAxes, bbox=dict(boxstyle='square', facecolor='tab:blue', alpha=0.1))
 
 axis_4.set_title(f'Balanced Test Split', fontweight='bold')
@@ -42,7 +42,7 @@ axis_4.tick_params(axis='x', rotation=90)
 axis_4.set_ylim((0, 900))
 axis_4.set_yticks(np.arange(0, 901, 150))
 axis_4.bar(df_balanced['sense_final'].value_counts().index, df_balanced['sense_final'].value_counts(), color='tab:orange')
-axis_4.text(h_offset, v_offset, 'Total Implicit DR:  4,132 \n One-Sense Implitcit DR:  4,132 \n Two-Senses Implicit DR:         0 ',
+axis_4.text(h_offset, v_offset, 'Total Implicit DR:  4,102 \n One-Sense Implitcit DR:  4,102 \n Two-Senses Implicit DR:         0 ',
             ha='right', va='top', transform=axis_4.transAxes, bbox=dict(boxstyle='square', facecolor='tab:orange', alpha=0.1))
 
 plt.tight_layout()
